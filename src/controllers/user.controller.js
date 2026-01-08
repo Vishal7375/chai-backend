e8151d40956fb5d09ed2fc50eeb8d415f0f3a4c4
Vishal Kubavat <vkubavat77@gmail.com>
2026-01-08 15:16:24 +0530

    postman config

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,8 @@ import { ApiError } from "../utils/ApiError.js";
 import { User} from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from frontend
@@ -16,7 +18,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
     const  { fullname, email, username, password } = req.body;
-    console.log("email: ", email);
+    // console.log("email: ", email);
 
     if (
         [fullname, email, username, password].some((field) => field?.trim() === "")
@@ -24,7 +26,7 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
-    User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username}, {email}]
     })
 
@@ -33,7 +35,13 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // console.log("FILES:", req.files);
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
